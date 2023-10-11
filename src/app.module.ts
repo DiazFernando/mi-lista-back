f5bce4eb0e991b53440b0f235ae63d06b19cd403
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-10-10 21:48:02 -0300

    back proyecto entregable

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ListaModule } from './lista/lista.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      "type":"mysql",
+    "host":"localhost",
+    "port":3306,
+    "username":"root",
+    "password":"timo2906",
+    "database":"lista-prueba",
+    "entities":[`${__dirname}/**/**/**.entity{.ts,.js}`],
+    "synchronize":true
+    })
+    ,ListaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
