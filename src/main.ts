c96e6381dfb731bc1e40f178adc45562fb0621e1
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-11-09 21:31:01 -0300

    configuracion de la url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['http://localhost:3000'],
+    origin:['http://localhost:3000', 'https://lista-tarea-cd515.web.app'],
     methods:'GET,PUT,POST,DELETE,PATH',
     credentials:true
   });
